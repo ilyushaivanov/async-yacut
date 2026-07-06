177272ef90b9f2bb63d68d5bd37ef67c2a27003d
ilya ivanov <ilyushaivanov@gmail.com>
2026-07-06 19:01:00 +0300

    last

--- yacut/views.py
+++ yacut/views.py
@@ -29,18 +29,16 @@ async def upload_file_to_disk(file_data, filename):
             if resp.status not in (200, 201):
                 return None
         publish_url = (
-            f'https://cloud-api.yandex.net/v1/disk/resources/publish?path={
-                path
-            }'
+            f'https://cloud-api.yandex.net/v1/disk/resources/publish?path='
+            f'{path}'
         )
         headers = {'Authorization': f'OAuth {Config.DISK_TOKEN}'}
         async with session.put(publish_url, headers=headers) as resp:
             if resp.status not in (200, 201):
                 return None
         download_url = (
-            f'https://cloud-api.yandex.net/v1/disk/resources/download?path={
-                path
-            }'
+            f'https://cloud-api.yandex.net/v1/disk/resources/download?path='
+            f'{path}'
         )
         headers = {'Authorization': f'OAuth {Config.DISK_TOKEN}'}
         async with session.get(download_url, headers=headers) as resp:
